da683b93d79afc268f6ef4072be05897875937a9
Nicolas DUDNICZENKO <ndudnicz@e1r7p1.42.fr>
2016-11-10 10:48:05 +0100

    setup.js mongodb done

--- setup.js
+++ setup.js
@@ -1,72 +1,50 @@
-var fs = require('fs')
+const fs = require('fs')
 ,config = JSON.parse(fs.readFileSync('./config.json'))
-,sqlite3 = require('sqlite3')
-,dbUsers = new sqlite3.Database('db/users.db')
-,dbBanned = new sqlite3.Database('db/banned.db')
-,dbUploads = new sqlite3.Database('db/uploads.db');
+,Mongo = require('mongodb')
+,assert = require('assert')
+,bcrypt = require('bcrypt');
 
-dbUploads.serialize(() => {
-	dbUploads.run("DROP TABLE IF EXISTS uploads;", (err) => {
-		if (err) {
-			console.log(err);
-		}
-	});
-	dbUploads.run("CREATE TABLE IF NOT EXISTS uploads (\
-		id INTEGER PRIMARY KEY AUTOINCREMENT,\
-		path TEXT,\
-		filename TEXT,\
-		ip TEXT,\
-		reported INTEGER,\
-		timestamp INTEGER,\
-		download_number INTEGER DEFAULT 0);", (err) => {
+var promise = new Promise((resolve, reject) => {
+	let url = config['mongodbURL'] + config['mongodbDB'];
+	Mongo.connect(url, (err, db) => {
+		assert.equal(null, err);
+		resolve(db);
 	});
 });
-dbBanned.serialize(() => {
-	dbBanned.run("DROP TABLE IF EXISTS banned;", (err) => {
-		if (err) {
-			console.log(err);
-		}
+
+function deleteOne(collection, request, callback) {
+	collection.deleteOne(request, (err, res) => {
+		assert.equal(null, err);
+		console.log("Admin deleted");
+		callback();
 	});
-	dbBanned.run("CREATE TABLE IF NOT EXISTS banned (\
-		id INTEGER PRIMARY KEY AUTOINCREMENT,\
-		ip TEXT,\
-		timestamp INTEGER,\
-		UNIQUE (ip));", (err) => {
+}
+
+function insertOne(collection, data, callback) {
+	collection.insertOne(data, (err, res) => {
+		assert.equal(null, err);
+		console.log("Admin created");
+		callback();
 	});
-});
-dbUsers.serialize(() => {
-	dbUsers.run("DROP TABLE IF EXISTS users;", (err) => {
-		if (err) {
-			console.log(err);
-		}
-	});
-	dbUsers.run("CREATE TABLE IF NOT EXISTS users (\
-		id INTEGER PRIMARY KEY AUTOINCREMENT,\
-		login TEXT,\
-		pass TEXT,\
-		token TEXT);",
-		(err) => {
-			if (err) {
-				return console.error(err);
-			}
-		}
-	);
-	var bcrypt = require('bcrypt');
-	var salt = bcrypt.genSaltSync(10);
-	var hash = bcrypt.hashSync(config["adminPassword"], salt);
-	dbUsers.run("INSERT INTO users VALUES (?, ?, ?, ?);",
-		[
-			null,
-			config["adminLogin"],
-			hash,
-			'toto'
-		], (err) => {
-			if (err) {
-				console.error(err);
-			}
-		}
-	);
-	dbUsers.all("SELECT * FROM users;", (err, rows) => {
-		console.log(rows);
+}
+
+promise.then(db => {
+	db.authenticate(config['mongodbUser'], config['mongodbPwd'], (err, res) => {
+		assert.equal(null, err);
+
+		let hash = bcrypt.hashSync(config['adminPassword'], bcrypt.genSaltSync(10))
+			,collection = db.collection('admin');
+
+		deleteOne(collection, {"_id": config['adminLogin']}, function() {
+			var data = {
+				"_id": config['adminLogin']
+				,"pwd": hash
+				,"token": "toto"
+			};
+			insertOne(collection, data, function() {
+				collection.find({}, (err, res) => {console.log("Setup done");});
+				db.close();
+			});
+		});
 	});
 });
